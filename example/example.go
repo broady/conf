df23a73fe14e2ae219aa66996b744d3faacd9bd0
Chris Broadfoot <cbro@golang.org>
2016-03-15 17:07:13 -0700

    example: add another example

--- example/example.go
+++ example/example.go
@@ -18,4 +18,11 @@ func main() {
 		conf.Default("bar"))
 
 	log.Print(foo)
+
+	xx, err := conf.Get(
+		conf.Env("FOO1"),
+		conf.Env("FOO2"))
+
+	// must set one of: environment variable FOO1, environment variable FOO2
+	log.Print(xx, err)
 }
